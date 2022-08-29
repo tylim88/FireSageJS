ce692eafdbc71b39e3016e22eda6d4040d9ae114
tylim <limteikyee@gmail.com>
2022-08-30 03:03:45 +0800

    fix set priority test

--- src/operations/setPriority.test.ts
+++ src/operations/setPriority.test.ts
@@ -12,94 +12,86 @@ describe('test set priority type', () => {
 				ref('a'),
 				1
 			)
-		}
-		;() => {
 			setPriority(
 				// @ts-expect-error
 				ref('b'),
 				1
 			)
-		}
-		;() => {
 			setPriority(
 				// @ts-expect-error
 				ref('b/c'),
 				1
 			)
-		}
-		;() => {
 			setPriority(
 				// @ts-expect-error
 				ref('b/d'),
 				1
 			)
-		}
-		;() => {
 			setPriority(
 				// @ts-expect-error
 				ref('b/h'),
 				1
 			)
+			setPriority(
+				// @ts-expect-error
+				ref('b/h/abc/i'),
+				1
+			)
+			setPriority(
+				// @ts-expect-error
+				ref('b/h/abc/l'),
+				1
+			)
+			setPriority(
+				// @ts-expect-error
+				ref('b/h/abc/m'),
+				1
+			)
+			setPriority(
+				// @ts-expect-error
+				ref('b/h/abc/p'),
+				1
+			)
+			setPriority(
+				// @ts-expect-error
+				ref('b/h/abc/l'),
+				1
+			)
+			setPriority(
+				// @ts-expect-error
+				ref('b/h/abc/m'),
+				1
+			)
+			setPriority(
+				// @ts-expect-error
+				ref('b/h/abc/p'),
+				1
+			)
+			setPriority(
+				// @ts-expect-error
+				ref('b/h/abc/s'),
+				1
+			)
+			setPriority(
+				// @ts-expect-error
+				ref('o'),
+				1
+			)
+			setPriority(
+				// @ts-expect-error
+				ref('q'),
+				1
+			)
+			setPriority(
+				// @ts-expect-error
+				ref('u'),
+				1
+			)
+			setPriority(
+				// @ts-expect-error
+				ref('w'),
+				1
+			)
 		}
-		setPriority(
-			// @ts-expect-error
-			ref('b/h/abc/i'),
-			1
-		)
-		setPriority(
-			// @ts-expect-error
-			ref('b/h/abc/l'),
-			1
-		)
-		setPriority(
-			// @ts-expect-error
-			ref('b/h/abc/m'),
-			1
-		)
-		setPriority(
-			// @ts-expect-error
-			ref('b/h/abc/p'),
-			1
-		)
-		setPriority(
-			// @ts-expect-error
-			ref('b/h/abc/l'),
-			1
-		)
-		setPriority(
-			// @ts-expect-error
-			ref('b/h/abc/m'),
-			1
-		)
-		setPriority(
-			// @ts-expect-error
-			ref('b/h/abc/p'),
-			1
-		)
-		setPriority(
-			// @ts-expect-error
-			ref('b/h/abc/s'),
-			1
-		)
-		setPriority(
-			// @ts-expect-error
-			ref('o'),
-			1
-		)
-		setPriority(
-			// @ts-expect-error
-			ref('q'),
-			1
-		)
-		setPriority(
-			// @ts-expect-error
-			ref('u'),
-			1
-		)
-		setPriority(
-			// @ts-expect-error
-			ref('w'),
-			1
-		)
 	})
 })
