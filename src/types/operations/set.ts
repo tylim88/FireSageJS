87080d54dc6643d55c7b1ac188c1e4a69158b8ed
tylim <limteikyee@gmail.com>
2022-09-03 06:37:12 +0800

    refactor set

--- src/types/operations/set.ts
+++ src/types/operations/set.ts
@@ -2,7 +2,7 @@ import { MetaType } from '../metaType'
 import { ErrorInvalidSetPriorityRef, ErrorIsPushOnlyAbleType } from './error'
 import {
 	GetAllPushAbleOnlyPaths,
-	ValidateRecordString as ReplaceNumericIndexRecordIfInputIsStringIndexRecord,
+	ValidateRecordString,
 	FindNestedWriteTypeFromFullPath,
 	IsParentRecordOrArray,
 } from '../utils'
@@ -17,10 +17,7 @@ export type IsValidSetValue<
 	T extends MetaType,
 	U extends (keyof T['flatten_write'] & string) | undefined,
 	V
-> = ReplaceNumericIndexRecordIfInputIsStringIndexRecord<
-	V,
-	FindNestedWriteTypeFromFullPath<T, U>
->
+> = ValidateRecordString<V, FindNestedWriteTypeFromFullPath<T, U>>
 
 export type IsValidSetRef<
 	T extends MetaType,
@@ -28,3 +25,30 @@ export type IsValidSetRef<
 > = U extends GetAllPushAbleOnlyPaths<T>
 	? ErrorIsPushOnlyAbleType<U>
 	: DatabaseReference<T, U>
+
+/**
+Writes data to this Database location.
+
+This will overwrite any data at this location and all child locations.
+
+The effect of the write will be visible immediately, and the corresponding events ("value", "child_added", etc.) will be triggered. Synchronization of the data to the Firebase servers will also be started, and the returned Promise will resolve when complete. If provided, the onComplete callback will be called asynchronously after synchronization has finished.
+
+Passing null for the new value is equivalent to calling remove(); namely, all data at this location and all child locations will be deleted.
+
+set() will remove any priority stored at this location, so if priority is meant to be preserved, you need to use setWithPriority() instead.
+
+Note that modifying data with set() will cancel any pending transactions at that location, so extreme care should be taken if mixing set() and transaction() to modify the same data.
+
+A single set() will generate a single "value" event at the location where the set() was performed.
+
+@param ref — The location to write to.
+ */
+export type Set = <
+	T extends MetaType,
+	U extends (keyof T['flatten_write'] & string) | undefined,
+	R extends DatabaseReference<T, U>,
+	V
+>(
+	ref: R extends never ? R : IsValidSetRef<T, U>,
+	value: V extends never ? V : IsValidSetValue<T, U, V>
+) => Promise<void>
