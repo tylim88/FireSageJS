172a7c5d0a4ef387f4d39b50d4c14e40d9590c26
tylim <limteikyee@gmail.com>
2023-05-14 14:44:58 +0800

    simplify logic

--- src/types/metaType/objectFlatten.ts
+++ src/types/metaType/objectFlatten.ts
@@ -15,11 +15,9 @@ export type DeepValue<
 	// ! P extends `${infer K extends keyof T}/${infer Rest extends DeepKey<T[K],Mode>}` causes TS to hang, why?
 > = P extends `${infer K}/${infer Rest}`
 	? T[(K extends `${infer R extends number}` ? R : K) & keyof T] extends infer S
-		? S extends unknown[] // * make S distributive
-			? never
-			: Rest extends RemoveLastSlash<DeepKey<S>>
+		? S extends S // * make S distributive
 			? DeepValue<S, Rest>
-			: never // impossible route
+			: never
 		: never // impossible route
 	: T[(P extends `${infer R extends number}` ? R : P) & keyof T]
 
